7bab1a443f7f26de5e777417d7b8b47164ad5580
Atharv26j13 <atharvlotusvalley@gmail.com>
2024-12-09 19:47:38 +0530

    Add files via upload

---
+++ Fun_with_str.py
@@ -0,0 +1,6 @@
+# String Operations
+# Slicing
+# a = (var[ starting letter(0 onwards) : Ending letter(0 onwards) : Skip counting number ])
+
+a = "Hello World"
+print(a[0::2])
---
+++ Input.py
@@ -0,0 +1,7 @@
+#   INPUT FUNCTION
+# Variable = input("question")
+
+a = input("Enter your first number-\n")
+b = input("Enter your second number-\n")
+ans = int(a)+int(b)
+print("\n\n"+str(ans))
